880f38d7b7305b77725a4cfa40a55ea025427e34
Nguyen Hoang <hoang1234cong@gmail.com>
2026-01-11 20:57:46 +0700

    feat(model): Add new properties and methods to author and movie

--- src/vn/MovieManagement/model/Movie.java
+++ src/vn/MovieManagement/model/Movie.java
@@ -5,23 +5,31 @@ public class Movie extends BaseObject{
     private String duration;
     private String date;
     private String code;
+    private int AuthorNumber;
+    private int id;
 
-    public Movie(String Name, String description, String link, 
-                String duration, String date, String code, int userID) {
+    public Movie(String Name, String description, String link, int AuthorNumber,
+                String duration, String date, String code, int userID, int id) {
         super(Name, description, userID);
         this.link = link;
         this.duration = duration;
         this.date = date;
         this.code = code;
+        this.AuthorNumber = AuthorNumber;
+        this.id = id;
     }
 
     public String  getLink() { return this.link; }
     public String getDuration() { return this.duration; }
     public String getDate() { return this.date; }
     public String getCode() { return this.code; }
+    public int getAuthorNumber() { return this.AuthorNumber; }
+    public int getID() { return this.id; }
 
     public void setLink(String link) { this.link = link; }
     public void setDuration(String duration) { this.duration = duration; }
     public void setDate(String date) { this.date = date; }
     public void setCode(String code) { this.code = code; }
+    public void setAuthorNumber(int AuthorNumber) { this.AuthorNumber = AuthorNumber; }
+    public void setID(int id) { this.id = id; }
 }
--- src/vn/MovieManagement/model/author.java
+++ src/vn/MovieManagement/model/author.java
@@ -4,19 +4,27 @@ public class author extends BaseObject{
     private int age;
     private String country;
     private String code;
+    private int AuthorNumber;
+    private int id;
     public author(String Name, String description, int age, String country, 
-                  String code, int userID) {
+                  String code, int userID, int AuthorNumber, int id) {
         super(Name, description, userID);
         this.age = age;
         this.country = country;
         this.code = code;
+        this.AuthorNumber = AuthorNumber;
+        this.id = id;
     }
 
     public int getAge() { return this.age; }
     public String getCountry() { return this.country; }
     public String getCode() { return this.code; }
+    public int getAuthorNumber() { return this.AuthorNumber; }
+    public int getID() { return this.id; }
 
     public void setAge(int age) { this.age = age;}
     public void setCountry(String country) { this.country = country; }
     public void setCode(String code) { this.code = code; }
+    public void setAuthorNumber(int AuthorNumber) { this.AuthorNumber = AuthorNumber; }
+    public void setID(int id) { this.id = id; }
 }
