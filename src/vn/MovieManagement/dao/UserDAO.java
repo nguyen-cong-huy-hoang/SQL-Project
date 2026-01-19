9dd36659a81aebacdc0f4668c55facc792b247c5
Nguyen Hoang <hoang1234cong@gmail.com>
2026-01-19 22:31:17 +0700

    fix(model,dao): Fix the user search error using SQL queries in the UserDAO class, and remove the UserID attribute in the BaseObject class

--- src/vn/MovieManagement/dao/UserDAO.java
+++ src/vn/MovieManagement/dao/UserDAO.java
@@ -3,11 +3,10 @@ package vn.MovieManagement.dao;
 import vn.MovieManagement.util.DBConnection;
 import java.sql.*;
 
-import javax.naming.spi.DirStateFactory.Result;
 
 public class UserDAO {
 
-    public int checkLogin(String user, String passWord) {
+    public static int checkLogin(String user, String passWord) {
         String sql = "SELECT id FROM users where username = ? AND password = ?";
         try(Connection conn = DBConnection.getConnection();
         PreparedStatement psmt = conn.prepareStatement(sql)) {
@@ -24,7 +23,7 @@ public class UserDAO {
         return -1;
     }
 
-    public boolean register(String user, String passWord) {
+    public static boolean register(String user, String passWord) {
         if(checkUserName(user)) return false;
         String sql = "INSERT INTO Users(username, password) VALUES(?, ?)";
         try(Connection conn = DBConnection.getConnection(); 
@@ -39,16 +38,19 @@ public class UserDAO {
         return true;
     }
 
-    public boolean checkUserName(String user) {
-        String sql = "SELECT id FROM Users WHERE username = user";
-        try(Connection conn = DBConnection.getConnection();
-        Statement stmt = conn.createStatement()) {
-            ResultSet rs = stmt.executeQuery(sql);
-            if(rs.next()) {
-                return true;
+    public static boolean checkUserName(String user) {
+        String sql = "SELECT id FROM Users WHERE username = ?";
+    
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement psmt = conn.prepareStatement(sql)) { 
+            psmt.setString(1, user); 
+            ResultSet rs = psmt.executeQuery();
+            if (rs.next()) {
+                return true; 
             }
-        } catch(SQLException e) {
-            System.err.println(e.getMessage());
+        } catch (SQLException e) {
+            System.err.println("Loi checkUser: " + e.getMessage());
+            e.printStackTrace();
         }
         return false;
     }
--- src/vn/MovieManagement/model/BaseObject.java
+++ src/vn/MovieManagement/model/BaseObject.java
@@ -3,17 +3,15 @@ package vn.MovieManagement.model;
 public class BaseObject {
     private String Name;
     private String description;
-    private int userID;
-    public BaseObject(String Name, String description, int userID) {
+    public BaseObject(String Name, String description) {
         this.Name = Name;
         this.description = description;
-        this.userID = userID;
     }
 
     public String getName() { return this.Name; }
     public String getDescription() { return this.description; }
-    public int getUserID() { return this.userID; }
+
     public void setName(String Name) { this.Name = Name; }
     public void setDescription(String description) { this.description = description; }
-    public void setUserID(int userID) { this.userID = userID; }
+
 }
