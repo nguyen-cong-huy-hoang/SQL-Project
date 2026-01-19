37336819e60a9620f87b638326166353ca9a07cd
Nguyen Hoang <hoang1234cong@gmail.com>
2026-01-19 22:27:01 +0700

    feat(util): Load the driver manually

--- src/vn/MovieManagement/util/DBConnection.java
+++ src/vn/MovieManagement/util/DBConnection.java
@@ -8,13 +8,17 @@ public class DBConnection {
     private static final String url = "jdbc:sqlite:data.db";
     
     public static Connection getConnection() {
+        Connection conn = null;
         try {
-            Connection conn = DriverManager.getConnection(url);
-            return conn;
-        }
-        catch(SQLException e) {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection(url);
+        }  catch (ClassNotFoundException e) {
+            System.err.println("SOS: Khong tim thay file jar SQLite trong thu muc lib!");
+            e.printStackTrace();
+        } catch(SQLException e) {
             System.err.println("database connection error: " + e.getMessage());
             return null;
         }
+        return conn;
     }
 }
