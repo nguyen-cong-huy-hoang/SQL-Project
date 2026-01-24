fdbf6c558241c0b46b63881ce1e7ffbe564c93a8
Nguyen Hoang <hoang1234cong@gmail.com>
2026-01-24 12:48:56 +0700

    refactor: separate business logic from data model

---
+++ src/vn/MovieManagement/service/MovieManagement.java
@@ -0,0 +1,63 @@
+package vn.MovieManagement.service;
+
+import vn.MovieManagement.model.Movie;
+import java.util.ArrayList;
+
+public class MovieManagement {
+    private int Size;
+    private ArrayList<Movie> movieManagement;
+    public MovieManagement() {
+        movieManagement = new ArrayList<>();
+        Size = 0;
+    }
+
+
+    public void add(int id, Movie movie) {
+        movieManagement.add(id, movie);
+        Size++;
+    }
+
+    public void remove(int id) {
+        if(Size == 0) return;
+        movieManagement.remove(id);
+        Size--;
+    }
+
+    public void clear() {
+        movieManagement.clear();
+        Size = 0;
+    }
+
+    public void print() {
+        String format = "| %-10s | %-15.15s | %-10s | %-12s | %-30.30s |%n";
+
+        String header = String.format(format, "CODE", "NAME", "DURATION", "DATE", "DESCRIPTION");
+
+        int tableLength = header.length() - System.lineSeparator().length();
+        String separator = "-".repeat(tableLength);
+
+        System.out.println(separator);
+        System.out.print(header);
+        System.out.println(separator);
+
+        if (movieManagement == null || movieManagement.isEmpty()) {
+            String msg = "DATA DOES NOT EXIST";
+            int padding = (tableLength - msg.length() - 2); 
+            System.out.printf("| %-" + padding + "s |%n", msg);
+        } else {
+            for (Movie m : movieManagement) {
+
+                    System.out.printf(format,
+                        m.getCode(),               
+                        m.getName(),                    
+                        m.getDuration(),         
+                        m.getID(),                
+                        String.valueOf(m.getDate()), 
+                        (m.getDescription() == null ? "" : m.getDescription()) 
+                    );
+                }
+            }
+        System.out.println(separator);
+    }
+
+}
---
+++ src/vn/MovieManagement/service/UserManagement.java
@@ -0,0 +1,37 @@
+package vn.MovieManagement.service;
+
+import vn.MovieManagement.model.User;
+import java.util.ArrayList;
+public class UserManagement {
+    private ArrayList<User> userManager;
+    private int size;
+    private static UserManagement Instance;
+
+    private UserManagement() {
+        userManager = new ArrayList<>();
+        size = 0;
+    }
+
+    public static UserManagement getInstance() {
+        if(Instance == null) {
+            return new UserManagement();
+        }
+        return Instance;
+    }
+
+    public void add(int id, User user) {
+        userManager.add(id, user);
+        size++;
+    }
+    public void remove(int id) {
+        userManager.remove(id);
+        size--;
+    }
+
+    public void clear() {
+        userManager.clear();
+        size = 0;
+    }
+
+    
+}
